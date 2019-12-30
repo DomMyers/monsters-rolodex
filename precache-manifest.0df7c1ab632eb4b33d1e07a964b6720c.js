1c2656d169f7a48bfc48a8f256fc4a1de805f927
dom myers <dom_myers@hotmail.com>
2019-12-30 14:09:18 +0000

    Updates

---
+++ precache-manifest.0df7c1ab632eb4b33d1e07a964b6720c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6051fe144edc8fdb1accc9324ea24e19",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a54a9ad12bb9cbf74f19",
+    "url": "/monsters-rolodex/static/css/main.f3cda408.chunk.css"
+  },
+  {
+    "revision": "b329ae1d6927d397d9c3",
+    "url": "/monsters-rolodex/static/js/2.8eea32d1.chunk.js"
+  },
+  {
+    "revision": "a54a9ad12bb9cbf74f19",
+    "url": "/monsters-rolodex/static/js/main.688dab0e.chunk.js"
+  },
+  {
+    "revision": "d78c7e54dca26e3c25e2",
+    "url": "/monsters-rolodex/static/js/runtime-main.aaf9d0bd.js"
+  }
+]);
